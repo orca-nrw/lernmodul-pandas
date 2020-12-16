4d724ede29e845e8316ab5f6f0461878138b89a8
Anne Giesen <anne.giesen@study.hs-duesseldorf.de>
2020-12-16 16:02:06 +0100

    add more tasks and explainations + section navigation

--- data/TaskReview.sql
+++ data/TaskReview.sql
@@ -11,10 +11,5 @@ INSERT INTO "TaskReview" VALUES (0,'DFP','Zum Erstellen eines Dataframes kannst 
 df = pd.read_sql_query(''SELECT * from netflix_titles'', con)
 con.close()');
 INSERT INTO "TaskReview" VALUES (1,'SC','',40,'20,10,40');
-INSERT INTO "TaskReview" VALUES (2,'DFS','Zum Erstellen eines Spark Dataframes aus einer Datenbanktabelle kann ein Pandas Dataframe als Zwischenspeicher verwendet werden.','SELECT * FROM netflix_titles;','con = sqlite3.connect(''taskReviewDatabase.db'')
-df_pd = pd.read_sql_query(''SELECT * from netflix_titles'', con)
-con.close()
-
-spark = SparkSession.builder.master(''local[1]'').getOrCreate()
-df = spark.createDataFrame(df_pd)');
+
 COMMIT;
