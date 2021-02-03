cf3b660255b40c00dad4da21a43dfff0574d93ff
Anne Giesen <anne.giesen@study.hs-duesseldorf.de>
2021-02-03 14:35:34 +0100

    add tasks section 5

--- vis_functions.py
+++ vis_functions.py
@@ -2,8 +2,11 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 def create_barplot_for_series(series):
-    if len(series) > 5:
+    if len(series) > 5 and len(series) < 10:
         bar, ax = plt.subplots(figsize = (10,5))
+    elif len(series) > 10:
+        bar, ax = plt.subplots(figsize = (20,8))
+        plt.xticks(rotation=90)
     sns.barplot(x = series.index, y = series)
     
 def create_heatmap(series):
